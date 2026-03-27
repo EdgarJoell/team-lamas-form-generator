c02e15109c11fb2dead0fbac6e23af75443e6f52
Edgar Zambrana <edgarjoel0920@gmail.com>
2026-03-26 21:39:43 -0400

    Add base app routes with wildcard handling that returns them back to the base page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Form} from './pages/form/form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'generate', component: Form },
+  { path: '**', redirectTo: 'generate' }
+];
