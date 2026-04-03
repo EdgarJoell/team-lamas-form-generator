21bef2ae3022f6f43eab893b7a423674a0ff2532
Edgar Zambrana <edgarjoel0920@gmail.com>
2026-04-03 00:00:40 -0400

    Add App Initializer to Project Config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
 import {
-  ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection
+  ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {InitializerService} from './services/initializer/initializer-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideZonelessChangeDetection(),
+    provideAppInitializer(() => {
+      const initService = inject(InitializerService);
+      return initService.initializeApp();
+    })
   ]
 };
